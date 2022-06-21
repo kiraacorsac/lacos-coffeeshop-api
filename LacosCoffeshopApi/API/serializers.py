668ef1be2ac73650fb40da6b00e7ad8a17ae8b2c
Filka, Ladislav (lf925u) <lf925u@att.com>
2022-06-21 13:36:19 +0200

    tags serializer

--- LacosCoffeshopApi/API/serializers.py
+++ LacosCoffeshopApi/API/serializers.py
@@ -12,6 +12,7 @@ from API.models import Foods, Tags
     #     return data        
 
 class TagSerializer (serializers.Serializer):
+    id = serializers.IntegerField()
     tag = serializers.CharField(max_length=60)
    
 
@@ -35,7 +36,17 @@ class FoodSerializer (serializers.Serializer):
 
 
     def create(self, validated_data):
-        return Foods.objects.create(**validated_data)
+        # return Foods.objects.create(**validated_data)
+        newfood = Foods.objects.create(name = validated_data["name"],
+        image = validated_data["image"],
+        likes = validated_data["likes"],
+        dislikes = validated_data["dislikes"],
+        fave = validated_data["fave"],
+        date = validated_data["date"])
+        # tags = validated_data["tags"])
+        print(validated_data["tags"])
+        newfood.tags.set([1,2])
+        return newfood
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
